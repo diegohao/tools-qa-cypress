186bb5cea7e32887a6295f0b476f7b7ebafd761a
Diego Henrique <58568683+diegohao@users.noreply.github.com>
2021-04-13 12:21:27 -0300

    Refatoração do locator de menu

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -1,12 +1,13 @@
 /// <reference types="cypress" />
 
-describe('Teste funcional de elements', () => {
-    
+import loc from '../support/locators'
+
+describe('Teste funcional de elements', () => {    
 
     it('Validar Text Box', () => {
         cy.visit('https://www.demoqa.com')
-        cy.get('.category-cards > :nth-child(1)').click()
-        cy.get(':nth-child(1) > .element-list > .menu-list > #item-0').should('exist').click()
+        cy.get(loc.MENU.ELEMENTS).click()
+        cy.get(loc.ELEMENTS.TEXT_BOX).should('exist').click()
         cy.get('#userName').type('Diego Henrique')
         cy.get('#userEmail').type('diego@teste.com')
         cy.get('#currentAddress').type('Rua Cypress, 91')
