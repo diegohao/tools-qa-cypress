79833bc0ddac896af59028842755f3ed42a4cf2a
Diego Henrique <58568683+diegohao@users.noreply.github.com>
2021-04-13 12:18:17 -0300

    Criação do locators.js

---
+++ cypress/support/locators.js
@@ -0,0 +1,20 @@
+const locators = {
+  MENU: {
+    ELEMENTS: '.category-cards > :nth-child(1)'
+    //FORMS:
+    //ALERTS_FRAME_WINDOWS:
+    //WIDGETS:
+    //INTERACTIONS:    
+  },
+  ELEMENTS: {
+    TEXT_BOX: ':nth-child(1) > .element-list > .menu-list > #item-0'
+    //CHECK_BOX:
+    //RADIO_BUTTON:
+    //WEB_TABLES:
+    //BUTTONS:
+    //LINKS:
+    //DYNAMIC_PROPERTIES:
+  }
+}
+
+export default locators;
